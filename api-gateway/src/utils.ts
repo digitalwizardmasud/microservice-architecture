f4c37636cc246de443b5fc2b385ba4cca4dc0ed6
Masud Rana <masud.pro.121@gmail.com>
2025-06-02 10:28:14 +0600

    fix(api-gateway): Middleware fix

--- api-gateway/src/utils.ts
+++ api-gateway/src/utils.ts
@@ -1,6 +1,16 @@
 import {Express, Request, Response} from 'express'
 import config from './config.json'
 import axios from 'axios'
+import middlewares from './middlewares'
+
+export const getMiddleware = (names:string[]) =>{
+    const midd =  names.map((name)=>{
+        return middlewares[name]
+    })
+   return midd.filter(item=>item != undefined)
+}
+
+
 
 export const createHandler = (hostname:string, path:string, method:string) =>{
     return async (req:Request, res:Response) => {
@@ -9,11 +19,19 @@ export const createHandler = (hostname:string, path:string, method:string) =>{
             req.params && Object.keys(req.params).forEach(param=>{
                 url = url.replace(`:${param}`, req.params[param])
             })
-            console.log(url, 'url')
+
             const {data} = await axios({
             method, 
             url,
-            data: req.body
+            data: req.body,
+            headers: {
+                origin: 'http://localhost:8081', // API Gateway origin
+                'x-user-id': req.headers['x-user-id'],
+                'x-user-email': req.headers['x-user-email'],
+                'x-user-role': req.headers['x-user-role'],
+                'x-user-name': req.headers['x-user-name'],
+                'user-agent': req.headers['user-agent']   
+            }
         })
         res.json(data)
         }catch(error){
@@ -27,13 +45,20 @@ export const createHandler = (hostname:string, path:string, method:string) =>{
     
 }
 
+
+
 export const configureRoutes = (app:any) => {
     Object.entries(config.services).forEach(([name, service])=>{
         const hostname = service.url 
         service.routes.forEach(route=>{
             route.methods.forEach((method)=>{
+                const middleware = getMiddleware(route.middleware || [])
                 const handler = createHandler(hostname, route.path, method)
-                app[method](`/api${route.path}`, handler)
+                if(middleware && middleware.length>0){
+                    app[method](`/api${route.path}`, middleware, handler)
+                }else{
+                    app[method](`/api${route.path}`, handler)
+                }
             })
         })
 
